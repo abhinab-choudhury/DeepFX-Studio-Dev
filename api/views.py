5e817d53ea0b8bd5138b7b88c20b4f599624424e
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2024-10-29 00:22:43 +0530

    ./api route added

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request): 
+  return render(request, "api/index.html")
+
+def health_check(request):
+  return JsonResponse({"status": "ok","message":"Server is Health"}, status=200)
